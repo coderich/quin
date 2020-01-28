2949b134e4be226a09649f944fe9f55de4ebba01
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-01-28 09:03:05 -0500

    Added prop name to all transforms/rules

--- src/service/rule.service.js
+++ src/service/rule.service.js
@@ -12,48 +12,48 @@ export const ImmutableRuleError = class extends RuleError {};
 export const NoRepeatRuleError = class extends RuleError {};
 export const DistinctRuleError = class extends RuleError {};
 
-export const allow = (...args) => (val, cmp = v => args.indexOf(v) === -1) => {
+export const allow = (...args) => Object.defineProperty((val, cmp = v => args.indexOf(v) === -1) => {
   if (val == null) return;
   if (cmp(val)) throw new AllowRuleError();
-};
+}, 'name', { value: 'allow' });
 
-export const deny = (...args) => (val, cmp = v => args.indexOf(v) > -1) => {
+export const deny = (...args) => Object.defineProperty((val, cmp = v => args.indexOf(v) > -1) => {
   if (val == null) return;
   if (cmp(val)) throw new DenyRuleError();
-};
+}, 'name', { value: 'deny' });
 
 export const range = (min, max) => {
   if (min == null) min = undefined;
   if (max == null) max = undefined;
 
-  return (val, cmp = v => Number.isNaN(v) || v < min || v > max) => {
+  return Object.defineProperty((val, cmp = v => Number.isNaN(v) || v < min || v > max) => {
     if (val == null) return;
     if (cmp(Number(val))) throw new RangeRuleError();
-  };
+  }, 'name', { value: 'range' });
 };
 
-export const email = () => (val, cmp = v => !isEmail(v)) => {
+export const email = () => Object.defineProperty((val, cmp = v => !isEmail(v)) => {
   if (val == null) return;
   if (cmp(val)) throw new EmailRuleError();
-};
+}, 'name', { value: 'email' });
 
 export const required = () => Object.defineProperty((val, cmp = v => v == null) => {
   if (cmp(val)) throw new RequiredRuleError();
 }, 'name', { value: 'required' });
 
-export const selfless = () => (val, cmp = v => false) => {
+export const selfless = () => Object.defineProperty((val, cmp = v => false) => {
   if (val == null) return;
   if (cmp(val)) throw new SelflessRuleError();
-};
+}, 'name', { value: 'selfless' });
 
-export const immutable = () => (val, cmp = v => false) => {
+export const immutable = () => Object.defineProperty((val, cmp = v => false) => {
   if (cmp(val)) throw new ImmutableRuleError();
-};
+}, 'name', { value: 'immutable' });
 
-export const norepeat = () => (val, cmp = v => false) => {
+export const norepeat = () => Object.defineProperty((val, cmp = v => false) => {
   if (cmp(val)) throw new NoRepeatRuleError();
-};
+}, 'name', { value: 'norepeat' });
 
-export const distinct = () => (val, cmp = v => false) => {
+export const distinct = () => Object.defineProperty((val, cmp = v => false) => {
   if (cmp(val)) throw new DistinctRuleError();
-};
+}, 'name', { value: 'distinct' });
--- src/service/transform.service.js
+++ src/service/transform.service.js
@@ -1,8 +1,8 @@
-export const lowerCase = () => (val, fn = v => v.toLowerCase()) => fn(val);
-export const upperCase = () => (val, fn = v => v.toUpperCase()) => fn(val);
-export const titleCase = () => (val, fn = v => v.replace(/\w\S*/g, w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())) => fn(val);
-export const sentenceCase = () => (val, fn = v => v.charAt(0).toUpperCase() + v.slice(1)) => fn(val);
-export const dedupe = () => (val, fn = v => [...new Set(v)]) => fn(val);
+export const lowerCase = () => Object.defineProperty((val, fn = v => v.toLowerCase()) => fn(val), 'name', { value: 'lowerCase' });
+export const upperCase = () => Object.defineProperty((val, fn = v => v.toUpperCase()) => fn(val), 'name', { value: 'upperCase' });
+export const titleCase = () => Object.defineProperty((val, fn = v => v.replace(/\w\S*/g, w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())) => fn(val), 'name', { value: 'titleCase' });
+export const sentenceCase = () => Object.defineProperty((val, fn = v => v.charAt(0).toUpperCase() + v.slice(1)) => fn(val), 'name', { value: 'sentenceCase' });
+export const dedupe = () => Object.defineProperty((val, fn = v => [...new Set(v)]) => fn(val), 'name', { value: 'dedupe' });
 
 // export const createdAt = val => val.charAt(0).toUpperCase() + val.slice(1);
 // export const updatedAt = val => val.charAt(0).toUpperCase() + val.slice(1);
