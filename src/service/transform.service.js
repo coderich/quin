866dda7cc338ec96b235b27c9abc26e94f0e3b7d
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-01-27 22:26:21 -0500

    added dedupe

--- src/service/transform.service.js
+++ src/service/transform.service.js
@@ -2,7 +2,7 @@ export const lowerCase = () => (val, fn = v => v.toLowerCase()) => fn(val);
 export const upperCase = () => (val, fn = v => v.toUpperCase()) => fn(val);
 export const titleCase = () => (val, fn = v => v.replace(/\w\S*/g, w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())) => fn(val);
 export const sentenceCase = () => (val, fn = v => v.charAt(0).toUpperCase() + v.slice(1)) => fn(val);
+export const dedupe = () => (val, fn = v => [...new Set(v)]) => fn(val);
 
-export const dedupe = () => (val, fn = v => v.toLowerCase()) => fn(val);
-export const createdAt = val => val.charAt(0).toUpperCase() + val.slice(1);
-export const updatedAt = val => val.charAt(0).toUpperCase() + val.slice(1);
+// export const createdAt = val => val.charAt(0).toUpperCase() + val.slice(1);
+// export const updatedAt = val => val.charAt(0).toUpperCase() + val.slice(1);
--- test/service/transform.service.test.js
+++ test/service/transform.service.test.js
@@ -6,5 +6,6 @@ describe('TransformService', () => {
     expect(TransformService.upperCase()('RiChArD')).toBe('RICHARD');
     expect(TransformService.titleCase()('now is the time for all good men to come to the aid of their country')).toBe('Now Is The Time For All Good Men To Come To The Aid Of Their Country');
     expect(TransformService.sentenceCase()('now is the time for all good men to come to the aid of their country')).toBe('Now is the time for all good men to come to the aid of their country');
+    expect(TransformService.dedupe()(['a', 'b', 'a', 'c'])).toEqual(['a', 'b', 'c']);
   });
 });
